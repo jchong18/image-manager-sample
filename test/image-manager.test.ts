23e4026fda91cf6ca2a58160ac62d39215bb4f4f
jchong18 <jhchong1018@gmail.com>
2023-06-22 08:25:32 +0900

    Initial commit

---
+++ test/image-manager.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as ImageManager from '../lib/image-manager-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/image-manager-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new ImageManager.ImageManagerStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
